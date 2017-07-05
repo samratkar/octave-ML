c7982be5cc2cfba7e2459763e980a00c2c0fb35d
samratk@gmail.com <samratk>
2017-07-05 13:02:49 +0530

    creating feature matrix

--- machine-learning-ex6/ex6/emailFeatures.m
+++ machine-learning-ex6/ex6/emailFeatures.m
@@ -10,6 +10,7 @@ n = 1899;
 % You need to return the following variables correctly.
 x = zeros(n, 1);
 
+
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return a feature vector for the
 %               given email (word_indices). To help make it easier to 
@@ -48,7 +49,10 @@ x = zeros(n, 1);
 %
 %
 
-
+  for i = 1 : length(word_indices)
+    x(word_indices(i))=1;
+  endfor
+    
 
 
 
